a22d383359fbdadff0c439708ee02dfa7f727013
MVBharat <manebharat85@gmail.com>
2018-11-26 18:47:17 +0530

    Basic Math

--- test.js
+++ test.js
@@ -105,3 +105,36 @@ console.log("Ceiling of "+decimal + " is " +ceil)
 var number = 2
 var num = Math.log(number)
 console.log("Logarithm of "+ number + " is " +num)
+
+// Maximum and Mininum
+var max = 50;
+var min = 2;
+var m1= Math.max(max, min)
+var m2= Math.min(max, min)
+console.log("Maximum of "+ max + " and "+ min + " is " + m1)
+console.log("Mininum of "+ max + " and "+ min +" is " + m2)
+
+// Power 
+var pow = 4
+var p = Math.pow(pow, 2)
+console.log("Power of "+ pow + " 2 is " +p)
+
+// Random
+var random = Math.random()
+console.log("Random num is  "+ random)
+
+function randomNumberBetween(max, min) {
+	return Math.floor(Math.random(max, min) * (max- min + 1), min)
+}
+var randoms = randomNumberBetween(max, min)
+console.log("Random value is " + randoms)
+
+
+// Round off
+var round = Math.round(pi)
+console.log("Round off "+ pi + " is " +round)
+
+// Square root of 
+var square = 44
+var squaroot = Math.sqrt(square)
+console.log("Square root of "+ square + " is " + squaroot)
