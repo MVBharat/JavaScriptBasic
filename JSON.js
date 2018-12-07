11253344ca057f1b09cd7be9df8512768c48233c
MVBharat <manebharat85@gmail.com>
2018-12-07 11:12:07 +0530

    AJAX for JavaScript

--- JSON.js
+++ JSON.js
@@ -14,3 +14,17 @@ var json = '{"name":"sopheya", "age": 2, "weight": 22}'
 var dog = JSON.parse(json);
 console.log("\n **** Json parsing **** \n")
 console.log(dog);
+
+
+// AJAX
+
+var base = 'https://jsonplaceholder.typicode.com';
+// use fetch on the /posts route, and then pass the response along
+fetch(base + "/posts").then(function(response){
+	// with the response convert it to json , then pass it along
+	response.json().then(function(json1){
+		// print the json
+		console.log(json1)
+	});
+});
+
